5f0437cfc1dbb19dd934ad25e26d011783e17c70
Mbhc Formation <mbhc.formation@gmail.com>
2024-06-18 23:59:08 +0200

    Cleaned thor movements class (unused code)

--- Java_Exercices/codin-game-exercices/src/main/java/tn/mbhc/tudev/codingame/exercices/thor/ThorMovements.java
+++ Java_Exercices/codin-game-exercices/src/main/java/tn/mbhc/tudev/codingame/exercices/thor/ThorMovements.java
@@ -1,7 +1,8 @@
 package tn.mbhc.tudev.codingame.exercices.thor;
 
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Auto-generated code below aims at helping you parse the standard input
@@ -9,7 +10,8 @@ import java.util.TreeSet;
  * print initialTX and initialTY, if Thor seems not follow your orders.
  **/
 public class ThorMovements {
-
+	private static final Logger LOGGER = Logger.getAnonymousLogger();
+	
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in)) {
 			int lightX = in.nextInt(); // the X position of the light of power
@@ -20,42 +22,28 @@ public class ThorMovements {
 			// game loop
 			int remainingTurns = in.nextInt(); // The remaining amount of turns Thor can move. Do not remove this
 			while (remainingTurns > 0) {
-													// line.
+				StringBuilder commandBuilder = new StringBuilder();
+				// Check position on Y axis
+                if(initialTy > lightY) {
+                    commandBuilder.append("N");
+                    initialTy--;
+                } else if(initialTy < lightY) {
+                    commandBuilder.append("S");
+                    initialTy++;
+                }
 
-				while (remainingTurns > 0) {
-					StringBuilder commandBuilder = new StringBuilder();
-					// Check position on Y axis
-	                if(initialTy > lightY) {
-	                    commandBuilder.append("N");
-	                    initialTy--;
-	                } else if(initialTy < lightY) {
-	                    commandBuilder.append("S");
-	                    initialTy++;
-	                }
-
-	                // Check position on X axis
-	                if(initialTx > lightX) {
-	                    commandBuilder.append("W");
-	                    initialTx++;
-	                } else if(initialTx < lightX) {
-	                    commandBuilder.append("E");
-	                    initialTx--;
-	                }
-	                	
-	                new TreeSet<>((Integer o1, Integer o2) -> {
-						if(Math.abs(o1) > Math.abs(o2)) {
-							return 1;
-						} else if(Math.abs(o1) < Math.abs(o2)) {
-							return -1;
-						} else {
-							return 0;
-						}
-					});
-	                
-	                // A single line providing the move to be made: N NE E SE S SW W or NW
-	                System.out.println(commandBuilder.toString());
-					remainingTurns--;
-				}
+                // Check position on X axis
+                if(initialTx > lightX) {
+                    commandBuilder.append("W");
+                    initialTx++;
+                } else if(initialTx < lightX) {
+                    commandBuilder.append("E");
+                    initialTx--;
+                }
+                	
+                // A single line providing the move to be made: N NE E SE S SW W or NW
+                LOGGER.log(Level.INFO, commandBuilder::toString);
+				remainingTurns--;
 			}
 		}
 	}
